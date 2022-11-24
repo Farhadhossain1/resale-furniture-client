9febda3cf5f4a56e2abd67b9e20ac4d2827f0f4d
Forhad <farhadhossain01992@gmail.com>
2022-11-24 16:16:08 +0600

    LogIn section commited

--- src/LogInSignIn/Login/LogIn.js
+++ src/LogInSignIn/Login/LogIn.js
@@ -6,7 +6,7 @@ import './LogIn.css';
 
 
 const LogIn = () => {
-    const { register, handleSubmit } = useForm();
+    const { register,  formState: { errors }, handleSubmit } = useForm();
    
 
     const handleLogin = data =>{
@@ -25,19 +25,29 @@ const LogIn = () => {
                             <label className="label">
                             <span className="label-text text-xl font-semibold py-4">Email</span>
                             </label>
-                            <input type="email"  {...register("email")} placeholder="email" className="input input-bordered w-full "  />
+                            <input type="email"  {...register("email", 
+                            {  required: "Email Address is required"}
+                            )} placeholder="email" className="input input-bordered w-full "  />
+                            {errors.email && <p className='text-red-600 mt-2'>{errors.email?.message}</p>}
                             </div>
                             
                 <div className="form-control w-full ">
                             <label className="label my-3">
                             <span className="label-text text-xl font-semibold ">Password</span>
                             </label>
-                            <input type="password" {...register("password")} placeholder="password" className="input input-bordered w-full "   />
+                            <input type="password" {...register("password",
+                             { required: "Password Address is required",
+                             minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            },
+                             
+                            )} placeholder="password" className="input input-bordered w-full "   />
+                            {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                             <label className="label my-3">
                             <span className="label-text text-xl ">Forget password</span>
                             </label>
                             </div>
                             <input className='btn btn-success my-6  text-black w-full' value="Login" type="submit" />
+
             </form>
             <p>New to AllForm <Link className='text-primary' to="/signup"><strong>Create new Account</strong></Link></p>
             <div className="divider">OR</div>
