83b7ef6115ea379174003414e4eb7a9cd21c8873
Forhad <farhadhossain01992@gmail.com>
2022-11-28 20:30:13 +0600

    Set Up Dashboard all route and hook

--- src/Pages/Home/AdvertiseCard/AdvertiseCard.js
+++ src/Pages/Home/AdvertiseCard/AdvertiseCard.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
-const AdvertiseCard = () => {
+const AdvertiseCard = ({product}) => {
     return (
         <div>
             <h1 className=' font-bold text-4xl'>This is Product</h1>
+
+            <div className="card w-96 bg-base-100 shadow-xl mt-16 category-bg">
+                            <figure><img src={product.image} alt="Shoes" /></figure>
+                            <div className="card-body">
+                              <h2 className="card-title">
+                                <div className="font-bold text-2xl">{product.category_name}</div>
+                              </h2>
+                                  <div className='flex justify-between'>
+                                      <div>
+                                          <h3 className=' font-semibold'>Old Price : {product.old_price}</h3>
+                                      </div>
+                                      <div>
+                                          <h3 className=' font-semibold'>Origan Price: {product.origan_price}</h3>
+                                      </div>
+                                  </div>
+                                  <div>
+                                      <h3 className=' font-semibold'> Status : {product.status}</h3>
+                                  </div>
+                    
+                            </div>
+                          </div>
+
+
         </div>
     );
 };
---
+++ src/routes/BuyerRoute/BuyerRoute.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
+import useBuyer from '../../hook/useBuyer';
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useContext(AuthContext);
+    const [isBuyer, isBuyerLoading] = useBuyer(user?.email);
+    const location = useLocation();
+
+    if(loading || isBuyerLoading){
+        return <progress className="progress w-56"></progress>
+    }
+
+    if (user && isBuyer){
+        return children;
+    }
+
+    return <Navigate to="/login" state={{from: location}} replace></Navigate>;
+};
+
+export default PrivateRoute;
